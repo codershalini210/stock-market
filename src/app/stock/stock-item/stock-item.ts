7ab9529c271f27b75b2e1a4c1fb1f3dd75d2faec
shalini <s.rathore43@gmail.com>
2026-01-13 16:48:09 +0530

    stock item component

--- src/app/stock/stock-item/stock-item.ts
+++ src/app/stock/stock-item/stock-item.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 // import { NgClass } from "../../../../node_modules/@angular/common/types/_common_module-chunk";
 import { CommonModule } from '@angular/common';
+import { Stock } from '../../model/stock';
 @Component({
   selector: 'app-stock-item',
   imports: [CommonModule],
@@ -8,17 +9,26 @@ import { CommonModule } from '@angular/common';
   styleUrl: './stock-item.css',
 })
 export class StockItem {
- public name!: string; 
-  public code!: string;
-  public price!: number;
-  public previousPrice!: number;
-   public positiveChange!: boolean;
+//  public name!: string; 
+//   public code!: string;
+//   public price!: number;
+//   public previousPrice!: number;
+//    public positiveChange!: boolean;
+//     public favorite!: boolean;
+public stock !:Stock;
   constructor() { }
   ngOnInit() {
-    this.name = 'Test Stock Company';
-    this.code = 'TSC';
-    this.price = 85;
-    this.previousPrice = 90;
-     this.positiveChange = this.price >= this.previousPrice;
+    // this.name = 'Test Stock Company';
+    // this.code = 'TSC';
+    // this.price = 85;
+    // this.previousPrice = 90;
+    // this.favorite = false;
+    //  this.positiveChange = this.price >= this.previousPrice;
+    this.stock = new Stock("Test Stock Company","TSC",80,95)
+  }
+   toggleFavorite(event :any) {
+    console.log("event is -"+event)
+    // this.favorite = !this.favorite;
+    this.stock.favorite = !this.stock.favorite
   }
 }
